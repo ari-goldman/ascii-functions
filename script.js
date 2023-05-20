cb558d243465a009d3ea56926b607d2a3fb8cb96
ari-goldman <112101394+ari-goldman@users.noreply.github.com>
2023-05-20 08:18:52 -0600

    more funcy '

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ const pi = Math.PI;
 const container = document.getElementById('container')
 functions = [
     ["sin(x)", (x) => { return Math.sin(x)}],
+    ["sin(.25x)cos(4x)", (x) => { return Math.sin(.25 * x) * Math.cos(4 * x) }],
     ["tan(.5x)", (x) => { return Math.tan(.5 * x)}],
-    ["(3 * cos(.5x)) % 2", (x) => { return 3 * Math.cos(.5 * x) % 2}],
+    ["3cos(.5x) mod 2", (x) => { return 3 * Math.cos(.5 * x) % 2}],
     ["sin(x) - .5cos(.5x) - .25sin(.25x)", (x) => { return Math.sin(x) - .5 * Math.cos(.5 * x) + .25 * Math.sin(.25 * x)}],
     ["5th degree fourier of a square wave", (x) => { x /= 4; return 4 / pi * (Math.sin(pi * x) + Math.sin(pi * x * 3) / 3 + Math.sin(pi * x * 5) / 5 + Math.sin(pi * x * 7) / 7)}],
 ];
-const funcnum = Math.floor(Math.random() * functions.length);
+var symbols = ['#', '&', '%', '@']
+
+const symbol = symbols[0] //symbols[Math.floor(Math.random() * symbols.length)]; -- only the hashtag looks good...
+const funcs = functions[Math.floor(Math.random() * functions.length)]; //Math.floor(Math.random() * functions.length);
+
 var lines = [];
 var texts = [];
 var numlines, chars;
-var bounds = [-2 * Math.PI, 2 * Math.PI];
 
-var speed = 100;
+let randoffset =2 * Math.PI * Math.random(); 
+var bounds = [randoffset, 4 * Math.PI + randoffset];
+
+var speed = Math.random() * 101 + 100;
 
 const close = (x, y) => {
     return Math.abs((x - y) / y) <= 0.1;
@@ -32,7 +39,7 @@ const plot = (xMin, xMax) => {
 
 const func = (x) => {
     // return 10 * (Math.sin(2 * x) + Math.cos(x));
-    return 8 * functions[funcnum][1](x);
+    return 8 * funcs[1](x);
 }
 
 const graph = () => {
@@ -46,7 +53,7 @@ const graph = () => {
         for(let y = 1; y < lines.length - 2; y++){
             let adjy = y - Math.round(lines.length/2);
             if(close(pts[x],adjy) || Math.round(pts[x]) == adjy){
-                texts[y] += "#";
+                texts[y] += symbol;
             }else if(adjy == 0){
                 texts[y] += "-";
             }else{
@@ -79,7 +86,7 @@ const makeLines = () => {
         texts.push("");
     }
     
-    texts[0] = functions[funcnum][0];
+    texts[0] = funcs[0];
     texts[lines.length - 1] = 'ari-goldman | <a href="https://github.com/ari-goldman/" target="_blank">github</a>';
 
     lines[0].innerHTML = texts[0];
@@ -95,8 +102,8 @@ window.onresize = () => makeLines();
 
 var interval = setInterval(() => {
     let diff = (bounds[1] - bounds[0]);
-    bounds[0] += diff / chars * (speed / 100);
-    bounds[1] += diff / chars * (speed/ 100);
+    bounds[0] += diff / chars;
+    bounds[1] += diff / chars;
     graph();
-}, 100);
+}, 100 / (speed / 100));
 
