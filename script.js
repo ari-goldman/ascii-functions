da6b23e089e22dcba0cfb25a57f1c05a239026a0
ari-goldman <112101394+ari-goldman@users.noreply.github.com>
2023-05-19 23:20:07 -0600

    changed colors, added some preset functions and names of them

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const pi = Math.PI;
+
 const container = document.getElementById('container')
+functions = [
+    ["sin(x)", (x) => { return Math.sin(x)}],
+    ["tan(.5x)", (x) => { return Math.tan(.5 * x)}],
+    ["(3 * cos(.5x)) % 2", (x) => { return 3 * Math.cos(.5 * x) % 2}],
+    ["sin(x) - .5cos(.5x) - .25sin(.25x)", (x) => { return Math.sin(x) - .5 * Math.cos(.5 * x) + .25 * Math.sin(.25 * x)}],
+    ["5th degree fourier of a square wave", (x) => { x /= 4; return 4 / pi * (Math.sin(pi * x) + Math.sin(pi * x * 3) / 3 + Math.sin(pi * x * 5) / 5 + Math.sin(pi * x * 7) / 7)}],
+];
+const funcnum = Math.floor(Math.random() * functions.length);
 var lines = [];
 var texts = [];
 var numlines, chars;
@@ -22,22 +32,19 @@ const plot = (xMin, xMax) => {
 
 const func = (x) => {
     // return 10 * (Math.sin(2 * x) + Math.cos(x));
-    return 8 * (Math.sin(x) - .5 * Math.cos(.5 * x) + .25 * Math.sin(.25 * x))
+    return 8 * functions[funcnum][1](x);
 }
 
 const graph = () => {
-    for(y = 0; y < lines.length; y++){
+    for(y = 1; y < lines.length - 1; y++){
         texts[y] = "";
     }
-
+    
     let pts = plot(bounds[0], bounds[1]);
-
-    // console.log(pts);
+    
     for(let x = 0; x < chars; x++){
-        for(let y = 0; y < lines.length - 1; y++){
-            // console.log(x/10, func(x/10), y/10, close(x/10, y/10));
+        for(let y = 1; y < lines.length - 2; y++){
             let adjy = y - Math.round(lines.length/2);
-            // console.log(x, pts[x], Math.round(pts[x]), adjy)
             if(close(pts[x],adjy) || Math.round(pts[x]) == adjy){
                 texts[y] += "#";
             }else if(adjy == 0){
@@ -48,13 +55,12 @@ const graph = () => {
         }
     }
 
-    texts[lines.length - 1] = 'ari-goldman | <a href="google.com">github</a>';
     updateLines();
 }
 
 const updateLines = () => {
     for(let i = 0; i < texts.length; i++){
-        lines[i].innerHTML = texts[i] + "\u200B";
+        lines[i].innerHTML = texts[i];
     }
 }
 
@@ -62,17 +68,19 @@ const makeLines = () => {
     container.innerHTML = "";
     lines = [];
     texts = [];
-    numlines = Math.ceil(document.body.clientHeight / 23);
+    numlines = Math.ceil(document.body.clientHeight / 22);
     chars = Math.ceil(document.body.clientWidth / 10);
-
+    
     for(let i = 0; i < numlines; i++){
         let newLine = document.createElement('text');
         newLine.classList.add("ascii-line");
         container.appendChild(newLine);
         lines.push(newLine);
-        texts.push("-".repeat(chars));
+        texts.push("");
     }
-
+    
+    texts[0] = functions[funcnum][0];
+    texts[lines.length - 1] = 'ari-goldman | <a href="https://github.com/ari-goldman/ascii-functions/settings/pages">github</a>';
     graph();
 }
 
@@ -86,7 +94,6 @@ var interval = setInterval(() => {
     let diff = (bounds[1] - bounds[0]);
     bounds[0] += diff / chars * (speed / 100);
     bounds[1] += diff / chars * (speed/ 100);
-    console.log(bounds)
     graph();
 }, 100);
 
