a81ba6f03e1a75692843e3f6c6aa87a7ca87260d
ari-goldman <112101394+ari-goldman@users.noreply.github.com>
2023-05-21 00:46:33 -0600

    custom functions!

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const pi = Math.PI;
 
 const container = document.getElementById('container')
 functions = [
-    ["sin(x)", (x) => { return Math.sin(x)}],
-    ["sin(.25x)cos(4x)", (x) => { return Math.sin(.25 * x) * Math.cos(4 * x) }],
-    ["tan(.5x)", (x) => { return Math.tan(.5 * x)}],
-    ["3cos(.5x) mod 2", (x) => { return 3 * Math.cos(.5 * x) % 2}],
-    ["sin(x) - .5cos(.5x) - .25sin(.25x)", (x) => { return Math.sin(x) - .5 * Math.cos(.5 * x) + .25 * Math.sin(.25 * x)}],
-    ["5th degree fourier of a square wave", (x) => { x /= 4; return 4 / pi * (Math.sin(pi * x) + Math.sin(pi * x * 3) / 3 + Math.sin(pi * x * 5) / 5 + Math.sin(pi * x * 7) / 7)}],
+    "sin(x)",
+    "sin(x/2) * cos(4*x)",
+    "tan(x/2)",
+    "3*cos(x/2) mod 2",
+    "sin(x) - cos(x/2)/2 - sin(x/4)/4",
+    "4 / PI * (sin(PI*x/4) + sin(3*PI*x/4)/3 + sin(5*PI*x/4)/5 + sin(7*PI*x/4)/7)"
+    // ["5th degree fourier of a square wave", (x) => { x /= 4; return 4 / pi * (Math.sin(pi * x) + Math.sin(pi * x * 3) / 3 + Math.sin(pi * x * 5) / 5 + Math.sin(pi * x * 7) / 7)}],
 ];
 var symbols = ['#', '&', '%', '@']
 
 const symbol = symbols[0] //symbols[Math.floor(Math.random() * symbols.length)]; -- only the hashtag looks good...
-const funcs = functions[Math.floor(Math.random() * functions.length)]; //Math.floor(Math.random() * functions.length);
 
 var lines = [];
 var texts = [];
 var numlines, chars;
 
-let randoffset =2 * Math.PI * Math.random(); 
+let randoffset = 2 * Math.PI * Math.random();
 var bounds = [randoffset, 4 * Math.PI + randoffset];
 
 var speed = Math.random() * 101 + 100;
@@ -28,35 +28,63 @@ const close = (x, y) => {
 }
 
 const plot = (xMin, xMax) => {
+    let f = parse(lines[0].innerText);
+
     var pts = [];
     let inc = (xMax - xMin) / chars;
-    for(let x = xMin; x < xMax; x += inc){
-        pts.push(func(x));
+    for (let x = xMin; x < xMax; x += inc) {
+        try {
+            pts.push(-8 * eval(f.replace(/x/g, x)));
+        } catch (error) {
+            pts.push(NaN);
+        }
     }
 
     return pts;
 }
 
-const func = (x) => {
-    // return 10 * (Math.sin(2 * x) + Math.cos(x));
-    return 8 * funcs[1](x);
+const parse = (f) => {
+    f = f.replace(/E/g, "Math.E")
+        .replace(/PI/g, "Math.PI")
+
+        .replace(/\^/g, "**")
+        .replace(/mod/g, "%")
+        .replace(/ln/g, "Math.log")
+
+        .replace(/sin/g, "Math.sin")
+        .replace(/cos/g, "Math.cos")
+        .replace(/tan/g, "Math.tan")
+
+        .replace(/abs/g, "Math.abs")
+        .replace(/ceil/g, "Math.ceil")
+        .replace(/floor/g, "Math.floor")
+        .replace(/round/g, "Math.round")
+        .replace(/rand/g, "Math.random()");
+
+    return f;
 }
 
+// const func = (x) => {
+//     // return 10 * (Math.sin(2 * x) + Math.cos(x));
+//     return 8 * funcs[1](x);
+// }
+
 const graph = () => {
-    for(y = 1; y < lines.length - 1; y++){
+    for (y = 1; y < lines.length - 1; y++) {
         texts[y] = "";
     }
-    
+
     let pts = plot(bounds[0], bounds[1]);
     
-    for(let x = 0; x < chars; x++){
-        for(let y = 1; y < lines.length - 2; y++){
-            let adjy = y - Math.round(lines.length/2);
-            if(close(pts[x],adjy) || Math.round(pts[x]) == adjy){
+    for (let x = 0; x < chars; x++) {
+        if(pts[0] === NaN) break;
+        for (let y = 1; y < lines.length - 2; y++) {
+            let adjy = y - Math.round(lines.length / 2);
+            if (close(pts[x], adjy) || Math.round(pts[x]) == adjy) {
                 texts[y] += symbol;
-            }else if(adjy == 0){
+            } else if (adjy == 0) {
                 texts[y] += "-";
-            }else{
+            } else {
                 texts[y] += "&nbsp;";
             }
         }
@@ -66,7 +94,7 @@ const graph = () => {
 }
 
 const updateLines = () => {
-    for(let i = 1; i < texts.length - 1; i++){
+    for (let i = 1; i < texts.length - 1; i++) {
         lines[i].innerHTML = texts[i];
     }
 }
@@ -77,8 +105,14 @@ const makeLines = () => {
     texts = [];
     numlines = Math.ceil(document.body.clientHeight / 22);
     chars = Math.ceil(document.body.clientWidth / 10);
+
+    lines[0] = document.createElement('span');
+    lines[0].role = 'textbox';
+    lines[0].id = 'func_box';
+    lines[0].contentEditable = 'true';
+    container.appendChild(lines[0])
     
-    for(let i = 0; i < numlines; i++){
+    for (let i = 1; i < numlines; i++) {
         let newLine = document.createElement('text');
         newLine.classList.add("ascii-line");
         container.appendChild(newLine);
@@ -86,24 +120,32 @@ const makeLines = () => {
         texts.push("");
     }
     
-    texts[0] = funcs[0];
-    texts[lines.length - 1] = 'ari-goldman | <a href="https://github.com/ari-goldman/" target="_blank">github</a>';
-
-    lines[0].innerHTML = texts[0];
-    lines[texts.length - 1].innerHTML = texts[texts.length - 1];
+    // texts[lines.length - 1] = 'ari-goldman | <a href="https://github.com/ari-goldman/" target="_blank">github</a>';
+    // lines[texts.length - 1].innerHTML = texts[texts.length - 1];
+    lines[lines.length - 1].classList.remove("ascii-line");
+    lines[lines.length - 1].classList.add("footer")
+    lines[lines.length - 1].innerHTML = 'ari-goldman | <a href="https://github.com/ari-goldman/" target="_blank">github</a>';
+    
     graph();
 }
 
 
 
-makeLines();
-
-window.onresize = () => makeLines();
 
 var interval = setInterval(() => {
     let diff = (bounds[1] - bounds[0]);
     bounds[0] += diff / chars;
     bounds[1] += diff / chars;
+    console.log(parse(lines[0].innerText));
     graph();
 }, 100 / (speed / 100));
 
+
+makeLines();
+lines[0].innerText = functions[Math.floor(Math.random() * functions.length)];
+
+window.onresize = () => {
+    let tempfunc = lines[0].innerText;
+    makeLines()
+    lines[0].innerText = tempfunc;
+};
